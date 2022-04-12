c2d2814cad6dd0703dbb7f15465a777acff7da38
Null Error [!] <103246803+xlorienull@users.noreply.github.com>
2022-04-12 17:27:03 +0000

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,13 @@
+:loop
+color a
+npm install @cliqz/adblocker-puppeteer
+npm install colors
+npm install cross-fetch
+npm install fs
+npm install node-bash-title
+npm install node-fetch
+npm install prompt-sync
+npm install puppeteer-extra-plugin-recaptcha
+npm install puppeteer-extra-plugin-stealth
+npm install unique-names-generator
+cls
---
+++ start.bat
@@ -0,0 +1,7 @@
+echo off
+color e
+cls
+:a
+node index.js
+goto a
+pause
